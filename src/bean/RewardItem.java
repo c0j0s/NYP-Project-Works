ae6f8e23e97a8e9a327b783eab5b4acd6a6346b1
Desmondkty <Desmondkty@mail.com>
2017-06-18 15:14:21 +0800

    add reward item javabean

---
+++ src/bean/RewardItem.java
@@ -0,0 +1,79 @@
+package bean;
+
+public class RewardItem {
+	private String rewardId,rewardTitle,rewardDescription,points,rewardAvailibility,rewardQuantity,imgUrl;
+	private char valid;
+	
+	public RewardItem(){
+		
+	}
+
+	public String getRewardId() {
+		return rewardId;
+	}
+
+	public void setRewardId(String rewardId) {
+		this.rewardId = rewardId;
+	}
+
+	public String getRewardTitle() {
+		return rewardTitle;
+	}
+
+	public void setRewardTitle(String rewardTitle) {
+		this.rewardTitle = rewardTitle;
+	}
+
+	public String getRewardDescription() {
+		return rewardDescription;
+	}
+
+	public void setRewardDescription(String rewardDescription) {
+		this.rewardDescription = rewardDescription;
+	}
+
+	public String getPoints() {
+		return points;
+	}
+
+	public void setPoints(String points) {
+		this.points = points;
+	}
+
+	public String getRewardAvailibility() {
+		return rewardAvailibility;
+	}
+
+	public void setRewardAvailibility(String rewardAvailibility) {
+		this.rewardAvailibility = rewardAvailibility;
+	}
+
+	public String getRewardQuantity() {
+		return rewardQuantity;
+	}
+
+	public void setRewardQuantity(String rewardQuantity) {
+		this.rewardQuantity = rewardQuantity;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public char getValid() {
+		return valid;
+	}
+
+	public void setValid(char valid) {
+		this.valid = valid;
+	}
+	
+}
+	
+	
+
+
