437b0df9c17f4bbbe8a3bd7f4e767ecaaa4e217e
joshualwk <joshlimwk@gmail.com>
2017-06-19 10:34:56 +0800

    layout

---
+++ src/Database/DBAO.java
@@ -0,0 +1,13 @@
+package Database;
+
+import java.sql.Connection;
+
+public class DBAO {
+	Connection con;
+	
+	public static String url = "jdbc:mysql://138.75.188.127/ffl";
+	public static String dbdriver = "com.mysql.jdbc.Driver";
+	public static String username = "root";
+	public static String password = "password";
+
+}
