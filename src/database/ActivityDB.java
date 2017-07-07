ce9e4b60fa1114dc1bedb771b16c8a1936645878
CJS <c.junsheng@hotmail.com>
2017-07-07 11:14:20 +0800

    combine rewardlist

--- src/database/ActivityDB.java
+++ src/database/ActivityDB.java
@@ -43,18 +43,18 @@ public class ActivityDB extends DBAO{
 				act.setActivityTime(rs.getString("activityTime"));
 				act.setValid(rs.getString("valid").charAt(0));
 				act.setActivityDay(rs.getString("activityDays"));
-<<<<<<< HEAD
+
 				act.setAccountId(rs.getString("accountId"));
 				act.setLikeCount(rs.getInt("likeCount"));
 				act.setDislikeCount(rs.getInt("dislikeCount"));
 				act.setCommentCount(rs.getString("postCount"));
-=======
+
 				act.setAccountName(rs.getString("givenName"));
 				act.setAccountId(rs.getString("accountId")); 
 				act.setLikeCount(rs.getInt("likeCount"));
 				act.setDislikeCount(rs.getInt("dislikeCount"));
 				//act.setCommentCount(rs.getString("commentCount")); you dun have this in your view yet
->>>>>>> f403f9f8c166a807dc0713728da0a5b694487908
+
 				System.out.println(rs.getString("imgUrl"));
 				act.setImgUrl(rs.getString("imgUrl"));
 				act.setOrganiserId(rs.getString("accountId"));
