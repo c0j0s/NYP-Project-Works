ff511c36cad52299bdb37a62f0ac482f91521682
CJS <c.junsheng@hotmail.com>
2017-07-26 01:05:47 +0800

    setup notification bean and db

---
+++ src/database/NotificationDB.java
@@ -0,0 +1,22 @@
+package database;
+
+import java.util.ArrayList;
+
+import bean.Notification;
+
+public class NotificationDB extends DBAO{
+
+	public NotificationDB() {
+		super();
+	}
+	
+	public ArrayList<Notification> getAccountNotifications(String accountId) {
+		ArrayList<Notification> list = new ArrayList<Notification>();
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		return list;
+	}
+}
